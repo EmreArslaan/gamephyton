bfe2d011faada3bd0097a3d34ff734e97b03a82d
EmreArslaan <150919810+EmreArslaan@users.noreply.github.com>
2024-01-16 14:20:24 +0100

    Update gameproject.py

--- gameproject.py
+++ gameproject.py
@@ -1,1 +1,21 @@
-.
+def give_options(x,y,z):
+    print("a):", x)
+    print("b):", y)
+    print("c):", z)
+    
+print("Hello! Welcome to my Quiz" "\n" "All Questions carries 10 marks each")
+ans = input("Are you ready to play (yes/no): ")
+a ="Note: wrtie answers! do not write option."
+score = 0
+total_questions = 4
+
+correct_ans =["python", "audi", "youtube", "albert einstein "]
+
+if ans.lower() == "yes":
+    print(a)
+    print("Question- What is the best Programming Language? ")
+    give_options("Python", "C", "Java" )
+    ans=input("&gt;&gt;&gt;")
+    if ans.lower() == correct_ans[0]:
+        score=score+1
+        print("Correct")
